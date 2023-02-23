1db2d9b46fd64c2fd671531f63c614f20a8c57c4
zgronska <zgronska@gmail.com>
2023-02-24 00:02:27 +0100

    Added a score bar, basic score calculations

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+const maxScore = 550;
+const form = document.getElementById("form");
+const resultEl = document.querySelector(".result");
+
 function getData(form) {
   const formData = new FormData(form);
+  let score = 0;
 
-  // iterate through entries...
-  for (let pair of formData.entries()) {
-    console.log(pair[0] + ": " + pair[1]);
+  // iterate through entries
+  for (let item of formData.entries()) {
+    // calculate score
+    score += Number(item[1]);
   }
+  console.log(score);
 }
 
-document.getElementById("form").addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   getData(e.target);
+  resultEl.style.display = "block";
 });
+
+function updateProgress(progress) {
+  const circle = document.querySelector(".circle");
+  const circumference = circle.getTotalLength();
+  const offset = circumference - (progress / 100) * circumference;
+
+  if (circle.getAttribute("stroke-dasharray")) {
+    circle.setAttribute("stroke-dasharray", `${progress}, 100`);
+  } else {
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.setAttribute("stroke-dasharray", `${progress}, 100`);
+  }
+
+  const percentage = document.querySelector(".percentage");
+  percentage.textContent = `${progress}%`;
+}
+
+updateProgress(15);
