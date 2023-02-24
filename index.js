1b1c9e9fdc8a4cb51b67656132fd3ed666b6f08d
zgronska <zgronska@gmail.com>
2023-02-24 14:10:13 +0100

    Made the calculator functional, added content

--- index.js
+++ index.js
@@ -1,39 +1,91 @@
+// Total possible score
 const maxScore = 550;
+// Starting score
+let userScore = 0;
+
+// Get elements from document
 const form = document.getElementById("form");
 const resultEl = document.querySelector(".result");
+const circle = document.querySelector(".circle");
+const percentage = document.querySelector(".percentage");
+const resultText = document.createElement("p");
 
+// Get user's input
 function getData(form) {
   const formData = new FormData(form);
-  let score = 0;
 
   // iterate through entries
   for (let item of formData.entries()) {
     // calculate score
-    score += Number(item[1]);
+    userScore += Number(item[1]);
   }
-  console.log(score);
 }
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  getData(e.target);
-  resultEl.style.display = "block";
-});
-
-function updateProgress(progress) {
-  const circle = document.querySelector(".circle");
+// Update the result circle
+function updateCircle(percent) {
   const circumference = circle.getTotalLength();
-  const offset = circumference - (progress / 100) * circumference;
 
   if (circle.getAttribute("stroke-dasharray")) {
-    circle.setAttribute("stroke-dasharray", `${progress}, 100`);
+    circle.setAttribute("stroke-dasharray", `${percent}, 100`);
   } else {
     circle.style.strokeDasharray = `${circumference} ${circumference}`;
-    circle.setAttribute("stroke-dasharray", `${progress}, 100`);
+    circle.setAttribute("stroke-dasharray", `${percent}, 100`);
   }
 
-  const percentage = document.querySelector(".percentage");
-  percentage.textContent = `${progress}%`;
+  percentage.textContent = `${percent}%`;
 }
 
-updateProgress(15);
+// Calculate survival chances
+function calculateSurvival(score) {
+  // Convert score to percentage
+  const percentScore = Math.round((100 * score) / maxScore);
+  // (100 * score) / maxScore;
+
+  // Fill the element displaying result
+  resultText.textContent = `You scored ${score} points! `;
+
+  // Render additional text based on the score
+  if (percentScore < 20) {
+    resultText.textContent += `YWell, at least you'll go out in a blaze of glory... or maybe just a blaze. Sorry, friend. Remember, it's not the size of the gun, it's how you use it.`;
+  } else if (percentScore < 40) {
+    resultText.textContent += `You're not quite ready to be a hero in the zombie apocalypse yet. Don't worry, even the most skilled survivors had to start somewhere. In the words of Joel from The Last of Us, 'endure and survive.'`;
+  } else if (percentScore < 60) {
+    resultText.textContent += `Not bad! You're like a zombie's worst nightmare... as long as that zombie isn't too hungry. Remember, aim for the head, always have an exit strategy, and listen for the sound of clickers.`;
+  } else if (percentScore < 80) {
+    resultText.textContent += `You're a survivor! You have a good chance of making it through the early stages of the apocalypse, but stay alert and be prepared for anything. And don't forget to share your tips with the rest of us!`;
+  } else if (percentScore < 100) {
+    resultText.textContent += `You're a pro at surviving! You have a strong chance of making it through the apocalypse, but don't get too overconfident.`;
+  } else if (percentScore === 100) {
+    resultText.textContent += `Congratulations, you're a survivor! You're like Joel, but with less emotional baggage. You know what it takes to make it through any challenge the apocalypse throws your way.`;
+  }
+
+  // Append the result to the appropriate element
+  resultEl.appendChild(resultText);
+
+  // Render the circle
+  updateCircle(percentScore);
+
+  // Make it visible
+  resultEl.style.display = "block";
+}
+
+// Reset everything
+function clearData() {
+  if (userScore) {
+    resultEl.style.display = "none";
+    resultEl.removeChild(resultText);
+    userScore = 0;
+    updateCircle(0);
+  }
+}
+
+// Display result
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Clear previous result
+  clearData();
+  // Get data from form
+  getData(e.target);
+  // Calculate result
+  calculateSurvival(userScore);
+});
